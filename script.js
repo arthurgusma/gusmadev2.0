6e24cb76070413db6ce8ecb77c5130a11c2aefed
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-01-26 19:42:12 -0300

    feat: add eventlistener to close menu when clicked

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function toggleTooltip() {
   
   tooltip.classList.toggle('active');
 }
+
+const checkbox = document.getElementById('burger-menu');
+
+document.body.addEventListener('click', function(event) {
+    if (event.target !== checkbox) {
+        checkbox.checked = false;
+    }
+});
